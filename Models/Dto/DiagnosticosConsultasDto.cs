d13f1a72f1ccbb51b5984f431fe4f4f959a8197f
Fernando <Fernando@DESKTOP-NE3EAQ5>
2025-08-27 12:50:27 -0300

    Se creó el último endpoint que trae las consultas de un paciente specífico

---
+++ Models/Dto/DiagnosticosConsultasDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Zismed_Apis.Models.Dto
+{
+    public class DiagnosticosConsultasDto
+    {
+        public int DiagnosticosConsultasId { get; set; }
+        public string? Nombre { get; set; }
+    }
+}
