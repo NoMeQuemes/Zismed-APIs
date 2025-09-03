11476d43bd8a39d2fe080808dd47118555bfd348
Fabricio <fkuasniski@excelenciadigital.net>
2025-09-03 08:58:55 -0300

    SP-193 agrego metodo buscar cie10 y crear consulta

---
+++ Models/DiagnosticoDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Zismed_Apis.Models;
+
+public partial class DiagnosticoDto
+{
+    public int DiagnosticosConsultasId { get; set; }
+
+    //public string Codigo { get; set; } = null!;
+
+    public string Nombre { get; set; } = null!;
+
+
+}
